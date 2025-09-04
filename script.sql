45130fa4efe20a8b0402ef7f85a35047b37d0821
david <loopesdavid@gmail.com>
2025-09-04 15:21:05 -0300

    criando o banco de dados

---
+++ script.sql
@@ -0,0 +1,15 @@
+-- CRIANDO  O BANCO DE DADOS
+CREATE DATABASE BDProjeto;
+
+USE BDProjeto;
+
+-- CRIANDO AS TABELAS DO BANCO DE DADOS	
+CREATE TABLE tbPedido(
+	codPedido int primary key auto_increment,
+    tamanhoTenis varchar (50),
+    valorTenis varchar (50),
+    valorQuantidade varchar (50),
+    valorTotal varchar (50)
+);
+-- CONSULTANDO A TABELA DO BANCO DE DADOS
+SELECT * FROM tbPedido;
